212087b6ac97b97fbf7203edb9dc75b0a3d71f6f
FathimaNN <fathimann.official@gmail.com>
2026-09-02 08:24:39 +0530

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,51 @@
+# print("hai")
+# name="farhan"
+# print(name)
+# a=input("enter name")
+# print("hello",a)
+# a=int(input("enter first number"))
+# b=int(input("enter second number"))
+# # print(a+b)
+# c=a-b
+# print(c)
+
+
+
+#arithematic operators
+# a=10
+# b=8
+# print(a+b)#add
+# print(a-b)#substraction
+# print(a*b)#multiplication
+# print(a/b)#divide
+# print(a//b)#floor division
+# print(a%b)#modulos
+# print(a**b)#power
+
+#datatype knd pidikan
+# a=10
+# print(type(a))
+# print(float(a))#ith oru data typeil nin veere oru datatypelk mattan
+
+#assignment operators
+# x=30
+# x+=10# x=x+10
+# x-=10# x=x-10
+# x*=10#x=x*10
+#
+# print(x)
+
+
+
+#comparison operators
+#== x==10 equal to
+#!= x!=10 not equalto
+#>  x>10  greaterthan
+#<  x<10  lessthan
+#>= x>=10 greaterthan or equalto
+#<= x<=10 lessthan or equalto
+
+x=5
+print(x>=5)
+
+
